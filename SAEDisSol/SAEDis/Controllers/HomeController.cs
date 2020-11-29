72ecd91ca8adae256af63d90af9e241480732e16
JairSalinas <bytestudiomx@gmail.com>
2020-11-29 14:07:07 -0600

    Se concluye con la instalacion de admin lte

--- SAEDisSol/SAEDis/Controllers/HomeController.cs
+++ SAEDisSol/SAEDis/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SAEDis.Models;
 
 namespace SAEDis.Controllers
 {
+    [Authorize] 
     public class HomeController : Controller
     {
+       
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
